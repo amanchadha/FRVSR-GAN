c0d8793de1cc3db627a3ff93b6da25376c79b9d0
Aman Chadha <Aman@Amans-iMac-Pro.local>
2019-11-03 01:30:23 -0700

    updated run.sh

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-cd ~/Documents/iSeeBetter/FR-SRGAN 
+# iSeeBetter: A Novel Approach to Video Super-Resolution using Adaptive Frame Recurrence and Generative Adversarial Networks
+# aman@amanchadha.com
+
 python3 checkTrain.py
 
-cd ~/Documents/iSeeBetter/FR-SRGAN
 python3 Test_iSeeBetter.py --video out_srf_original_random_sample.mp4
 
-cd ~/Documents/iSeeBetter/FR-SRGAN/SRGAN
+cd SRGAN
 python3 test_video.py --video ../out_srf_4_out_srf_original_random_sample.mp4
