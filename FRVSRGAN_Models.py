c11c32d3298df7aee912d6f039b4ffebed6542e7
Aman Chadha <Aman@Amans-iMac-Pro.local>
2019-12-02 19:35:34 -0800

    fixed typo in models

--- FRVSRGAN_Models.py
+++ FRVSRGAN_Models.py
@@ -137,7 +137,7 @@ class FRVSR(nn.Module):
         self.height = lr_height
         self.batch_size = batch_size
         self.fnet = FNet()
-        self.todepth = SpaceToDepth(FRVSRGAN.SRFactor)
+        self.todepth = SpaceToDepth(FRVSR.SRFactor)
         self.srnet = SRNet(FRVSR.SRFactor * FRVSR.SRFactor * 3 + 3)  # 3 is channel number
 
     # make sure to call this before every batch train.
